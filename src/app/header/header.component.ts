9a2cffbff6bbe026d936fc8d61e72a02fd113022
meena <meena30it@gmail.com>
2020-03-01 20:36:23 +0530

    role based authentication and redirect

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {AuthenticationService} from "../services/authentication.service";
+import {Role} from '../models/role';
 
 @Component({
   selector: 'app-header',
@@ -23,5 +24,9 @@ logout() {
         this.authenticationService.logout();
         this.router.navigate(['/login']);
     }
+
+get isAdmin() {
+        return this.currentUser && this.currentUser.role === Role.Admin;
+    }    
     
 }
